6814d8ac7315f1f3982098ef7bea98092f12e6e6
sreispipa <s.reis@pipastudios.com>
2021-11-07 00:49:08 -0300

    changing player clothing on interaction

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using General;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public List<SpriteRenderer> clothes;
+    public static PlayerController ME = null;
+
+    private void Awake()
+    {
+        if (ME == null)
+        {
+            ME = this;
+        }
+        else if (ME != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeClothes(string clothes)
+    {
+        foreach (var clt in this.clothes.Where(clt => clt.gameObject.name.Contains(clothes)))
+        {
+            clt.GetComponent<ChangeClothesColor>()
+                .ChangeColor(UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+            break;
+        }
+    }
+}
